b4d1b0cb8b9c64ab5d80cc60eba5554977d39eec
knowsteff <know.steff@gmail.com>
2014-02-07 12:48:17 +0100

    New Searchgraph

--- visualizations/GraphVisualization/test.js
+++ visualizations/GraphVisualization/test.js
@@ -136,17 +136,18 @@ function MakeGraph(){
 		},
 		vis:{
 			width:{value:5000},
-			height:{value:5000},
+			height:{value:5000}/*,
 			trans:{
 				x:{value:0},
 				y:{value:0}				
 			},
 			scale:{value:1}
+			*/
 		},
 		force:{//3000
 			charge:{value:-500},
-			gravity:{value:0.05},
-			linkDistance:{value:50}
+			//gravity:{value:0.05},
+			//linkDistance:{value:50}
 		}});
 
 
@@ -165,38 +166,83 @@ function MakeGraph(){
 	
 	// nodes
 	wordHistory.forEach(function(nodename,index){
-		if(g.build.show.nodeDict.hasOwnProperty("nid"+nodename) == false){
-			g.build.addNode("nid"+nodename);
-			g.build.setNodeProperties("nid"+nodename,{
-				xscale:2,yscale:2,text:index +": "+ nodename,fill:"red","font-size":"20px"
+		if(g.build.show.nodeDict.hasOwnProperty("nodeId"+nodename) == false){
+			g.build.addNode("nodeId"+nodename);
+			g.build.setNodeProperties("nodeId"+nodename,{
+				xscale:5,yscale:5,text:index +": "+ nodename,fill:"green","font-size":"20px"
 			}); 
+		}
+		else{
+		/*
+			var currentNodeProperty = g.build.getNodeProperties("nodeId"+nodename);
+			var newXscale = currentNodeProperty.xscale+1;
+			var newYscale = currentNodeProperty.yscale+1;
+			
+			g.build.setNodeProperties("nodeId"+nodename,{
+				xscale:newXscale,yscale:newYscale
+			}); 
+			*/
 		}
 	});
 	
 
-	
+	/*
 	// build links between nodes(keywords)
 	var sourceNode;
 	var targetNode;
 	var color = d3.scale.linear()
-		.domain([0, uniqueWordsResult.length])
+		.domain([uniqueWordsResult.length,0])
 		.range(["lime","blue"]);
 	var width = d3.scale.linear()
-		.domain([0, uniqueWordsResult.length])
+		.domain([uniqueWordsResult.length,0])
 		.range([2,10]);
 		
 	var trueCount = 0;	
 	for(nodeCount=1;nodeCount<wordHistory.length;nodeCount++){
 		sourceNode = wordHistory[nodeCount-1];
 		targetNode = wordHistory[nodeCount];
+		
 		if(sourceNode != targetNode){
 			trueCount++
-			g.build.addLink("nid"+nodeCount,"nid"+sourceNode,"nid"+targetNode);
-			g.build.setLinkProperties("nid"+nodeCount,{
-				distance:100,width:width(trueCount),color:color(trueCount)
+			g.build.addLink("nodeId"+nodeCount,"nodeId"+sourceNode,"nodeId"+targetNode);
+			g.build.setLinkProperties("nodeId"+nodeCount,{
+				distance:100,width:width(trueCount),color:color(trueCount),text:nodeCount,title:nodeCount
 			});   
 		}
 	}
+	*/
+	
+	
+	var color = d3.scale.linear()
+		.domain([wordHistory.length,0])
+		.range(["red","blue"]);
+	
+	//First Node for link
+	g.build.addNodeWithLink("nodeId"+wordHistory[0],"subNodeId"+0,"subLinkId"+0);
+	g.build.setLinkProperties("subLinkId"+0,{distance:10});
+	g.build.setNodeProperties("subNodeId"+0,{xscale:2,yscale:2,fill:"blue"});
+	//debug
+	//g.build.setNodeProperties("subNodeId"+0,{text:"subNodeId"+0,title:"subNodeId"+0});
+	
+	for(nodeCount=1;nodeCount<wordHistory.length;nodeCount++){
+		sourceNode = wordHistory[nodeCount-1];
+		targetNode = wordHistory[nodeCount];
+		
+		g.build.addNodeWithLink("nodeId"+targetNode,"subNodeId"+nodeCount,"subLinkId"+nodeCount);
+		g.build.setLinkProperties("subLinkId"+nodeCount,{distance:10});
+		//debug
+		//g.build.setNodeProperties("subNodeId"+nodeCount,{text:"subNodeId"+nodeCount,title:"subNodeId"+nodeCount});
+		g.build.setNodeProperties("subNodeId"+nodeCount,{fill:color(nodeCount)});
+		g.build.addLink("connectionLink"+nodeCount,"subNodeId"+(nodeCount-1),"subNodeId"+nodeCount);
+		
+		//if(sourceNode != targetNode){
+			g.build.setLinkProperties("connectionLink"+nodeCount,{
+				strength:0,color:color(nodeCount),text:nodeCount,title:nodeCount,width:3
+			});  
+		//}
+	}
+	g.build.setNodeProperties("subNodeId"+(wordHistory.length-1),{xscale:2,yscale:2,fill:"red"});
+	
 	
 	// get results for each keyword
 	var text="";
@@ -207,21 +253,21 @@ function MakeGraph(){
 			text = wordsWithResults[keyword].results[result].title;
 			text = text.length < 10 ? text : text.substring(0,9); 
 
-			g.build.addNodeWithLink("nid"+keyword,"lid"+lc,"lid"+lc);
-			g.build.setNodeProperties("lid"+lc,{
+			g.build.addNodeWithLink("nodeId"+keyword,"listId"+lc,"listId"+lc);
+			g.build.setNodeProperties("listId"+lc,{
 				xscale:1,yscale:1,fill:"fuchsia",text:text,title:wordsWithResults[keyword].results[result].title
 			}); 
 			
 			// get node Properties with user mouse interactions
 			if(wordsWithResults[keyword].userActions.hasOwnProperty(result)){
-				g.build.setNodeProperties("lid"+lc,{
+				g.build.setNodeProperties("listId"+lc,{
 					stroke:"black","stroke-width":"3"
 				}); 
 			}
 
-			g.build.setLinkProperties("lid"+lc,{
+			g.build.setLinkProperties("listId"+lc,{
 				width:2,
-				distance:50,
+				distance:75,
 				color:"purple"//,text:text
 			});   
 			
