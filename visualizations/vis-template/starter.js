c1f48667397a7961c55faab6090f599fe114029e
eduveas <eveas@know-center.at>
2014-02-24 11:59:25 +0100

    added template for visualizations

---
+++ visualizations/vis-template/starter.js
@@ -0,0 +1,16 @@
+var onReceiveData = function(queryTerms, processedData, items) {
+    var width = $(window).width()-30;
+    var height = 500;
+    var scape = facetScape(d3.select("body").select("div#facetScape"), width, height, processedData, items, queryTerms);
+}
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.method === 'newSearchTriggered') {
+            //requestPlugin(onReceiveData);
+        }
+    }
+);
+
+// get Data from Plugin
+requestPlugin(onReceiveData);
