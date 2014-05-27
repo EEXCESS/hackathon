dac632789b4fa3f5061ea23ed4975506d18b95b5
Jörg Schlötterer <joerg.schloetterer@uni-passau.de>
2014-05-27 16:05:07 +0200

    initial configuration

--- background/config.js
+++ background/config.js
@@ -4,4 +4,14 @@ var EEXCESS = EEXCESS || {};
     if (!localStorage['PP_BASE_URI']) {
         localStorage['PP_BASE_URI'] = 'http://localhost:8080/eexcess-privacy-proxy/';
     }
+
+    // init profile
+    if (!localStorage['profile.uuid']) {
+        localStorage['profile.uuid'] = randomUUID();
+    }
+
+    // init policy
+    localStorage['privacy.policy.geolocation'] = 6;
+    localStorage['privacy.policy.gender'] = 1;
+    localStorage['privacy.policy.history'] = 20;
 }());
