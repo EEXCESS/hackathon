404e5eda32e6ccd91d2f56dc05a3f82d32e95e4b
knowsteff <know.steff@gmail.com>
2014-05-28 12:09:54 +0200

    merge

---
+++ visualizations/GraphVisualization/Libs/BookmarkControl.js
@@ -0,0 +1,254 @@
+//bookmark control
+
+var currentSelectedBookmark = null;
+var bookmarkDict = {
+	bookmarks:{},
+	nodes:{}
+};
+
+//add new book mark
+$("#addbookmark").click(function(){
+    
+    //has bookmark a name?
+    var bookmarkname =  $("#newbookmarkname").val();
+    if(bookmarkname == ""){
+        $("#message").text("please get a bookmarkname!");
+        return;
+    }
+
+    //each bookmark is a unique name
+    var findsamename = $('#bookmark-body div[id="'+bookmarkname+'"]');
+    if(findsamename.length > 0){
+        $("#message").text("bookmark exists");
+        return;
+    }
+    
+    // add new bookmark
+    $("#bookmark-body").append(
+        '<div id="'+bookmarkname+'">'
+            +'<div class="bookmark">'
+                +'<div>'
+                    +'<input class="bookmarktext" type="text" readonly value="'+bookmarkname+'"></input>'
+                   // +'<button class="editbookmarkname">edit</button>'
+                    +'<input class="editcolor" disabled type="color" value="'+$("#newcolor").val()+'"></input>'
+                +'</div>'
+                +'<div class="workbookmarkshow" >'
+                    +'<button class="expanderbookmark">+</button>'        
+                    +'<button class="deletebookmark">x</button>'
+                +'</div>'
+                +'<div class="editbookmarkshow" style="display:none;">'
+                    +'<input class="bookmarknewtext" type="text" value="'+bookmarkname+'"></input>'
+                    +'<button class="cancelbookmarkname">cancel</button>'
+                +'</div>'
+            +'</div>'
+            +'<div class="bookmarkelement" style="display:none;">'
+               // +"No result"
+            +'</div>'
+        +'</div>'
+    );
+    
+    
+    // add events for bookmark
+    
+
+    //expand bookmarkelement
+    var ExpandBookmarkElement = function(event){
+        var bookmarlElement = "#"+event.data.bookmarkname+" .bookmarkelement";
+        
+        if($(bookmarlElement).css("display") == "none"){
+            //expand bookmark
+            //if($(bookmarlElement).children().length>0){
+            $(bookmarlElement).css("display","");
+            $("#"+bookmarkname+" .expanderbookmark").text("-");
+            //}else{
+            //    console.log("no bookmark element");
+            //}
+        }else{
+            //colapse bookmark
+            $("#"+bookmarkname+" .expanderbookmark").text("+");
+            $(bookmarlElement).css("display","none");
+        }
+    };
+    $("#"+bookmarkname+" .expanderbookmark").on("click",{bookmarkname:bookmarkname},ExpandBookmarkElement);
+    
+    
+    //edit bookmark color
+	/*
+    var EditBookmarlColor = function(event){
+        //change the color nodes
+        //todo...
+        //bookmarkDict.bookmarks[event.data.bookmarkname] ={};
+		
+        
+        //$("#"+bookmarkname+" .showable").css(
+        //    "background",$("#"+bookmarkname+" .editcolor").val()
+        //);
+        
+    };
+    $("#"+bookmarkname+" .editcolor").on("change",{bookmarkname:bookmarkname},EditBookmarlColor);
+    */
+    
+    //delete bookmark
+    var DeleteBookmark = function(event){
+        if (confirm("You want delete this bookmark?") == true) {
+            $("#"+event.data.bookmarkname).remove();
+            currentSelectedBookmark = null;
+			
+            //delete bookmarks from nodes
+
+			//delete nodes from graph
+			//var test = forceGraph.Graph.GetGraphData();	
+			var resultNodelistObj = bookmarkDict.bookmarks[event.data.bookmarkname];
+			Object.keys(resultNodelistObj).forEach(function(resultNodeId){
+				var currentBookmarkName = "Bookmark_"+resultNodeId+"_"+event.data.bookmarkname;
+				if(forceGraph.Graph.GetGraphData().data.dict.node.hasOwnProperty(currentBookmarkName)){
+					forceGraph.To.Object()
+						.Node.Delete(currentBookmarkName);
+				}
+					
+			});
+			forceGraph.To.Object().To.Graph().ReDraw();
+			
+			//work with dictionary 
+			var deletedNodes = bookmarkDict.bookmarks[event.data.bookmarkname];
+			Object.keys(deletedNodes).forEach(function(elementNode){
+
+				if(bookmarkDict.nodes.hasOwnProperty(elementNode)){
+					delete bookmarkDict.nodes[elementNode][event.data.bookmarkname];
+					if(bookmarkDict.nodes[elementNode].length == 0){
+						delete bookmarkDict.nodes[elementNode];
+					}
+				}
+			});
+
+            delete bookmarkDict.bookmarks[event.data.bookmarkname];
+			
+
+			
+            $("#message").text("bookmark deleted successfully!");
+        }else{
+            $("#message").text("cancel bookmark deleted!");
+        }
+    };
+    $("#"+bookmarkname+" .deletebookmark").on("click",{bookmarkname:bookmarkname},DeleteBookmark);
+    
+
+   
+    
+    //edit current bookmark
+    var EditCurrentBookmark = function(event){
+
+        var $buttonObj = $("#"+event.data.bookmarkname+" .editbookmarkname");
+        
+        if($buttonObj.text() == "edit"){
+            //user can editable bookmark name
+            $buttonObj.text("rename");
+            $("#"+event.data.bookmarkname+" .editbookmarkshow").css("display","");
+            $("#"+event.data.bookmarkname+" .workbookmarkshow").css("display","none");
+            
+        }else if($buttonObj.text() == "rename"){
+            //save edit new bookmark name
+            
+            
+            
+            $buttonObj.text("edit");
+            $("#"+event.data.bookmarkname+" .editbookmarkshow").css("display","none");
+            $("#"+event.data.bookmarkname+" .workbookmarkshow").css("display","");
+            
+            var newBookmarkName = $("#"+event.data.bookmarkname+" .bookmarknewtext").val();
+            
+            if(newBookmarkName == ""){
+                return;
+            }
+            
+            var findsamename = $('#bookmark-body div[id="' +newBookmarkName +'"]');
+            
+            //console.log(findsamename.length);
+            if(findsamename.length > 0){
+                //console.log("xxx");
+                //console.log($("#message"));
+                //console.log($("#message").length);
+
+                $("#message").text("gg");
+            }else{
+
+                //unbind the events
+                $("#"+event.data.bookmarkname).off("click");
+                $("#"+event.data.bookmarkname+" .editbookmarkname").off("click");
+                $("#"+event.data.bookmarkname+" .cancelbookmarkname").off("click");
+                $("#"+event.data.bookmarkname+" .deletebookmark").off("click");
+                //$("#"+event.data.bookmarkname+" .editcolor").off("change");
+                $("#"+event.data.bookmarkname+" .expanderbookmark").off("click");
+
+                
+                $("#"+event.data.bookmarkname).attr("id",newBookmarkName);
+                $("#"+newBookmarkName+" .bookmarktext").val(newBookmarkName);
+                
+                //bind the events
+                $("#"+newBookmarkName).on("click",{bookmarkname:newBookmarkName},CurrentBookmarkSelected);
+                $("#"+newBookmarkName+" .editbookmarkname")
+                    .on("click",{bookmarkname:newBookmarkName},EditCurrentBookmark);
+                $("#"+newBookmarkName+" .cancelbookmarkname")
+                    .on("click",{bookmarkname:newBookmarkName},CancelEditCurrentbookmark);
+                $("#"+newBookmarkName+" .deletebookmark")
+                    .on("click",{bookmarkname:newBookmarkName},DeleteBookmark);
+                //$("#"+newBookmarkName+" .editcolor")
+                //    .on("change",{bookmarkname:newBookmarkName},EditBookmarlColor);
+                $("#"+newBookmarkName+" .expanderbookmark")
+                    .on("click",{bookmarkname:newBookmarkName},ExpandBookmarkElement);
+                currentSelectedBookmark = newBookmarkName;
+                
+				var currentBookmark = bookmarkDict.bookmarks[event.data.bookmarkname];
+				delete bookmarkDict.bookmarks[event.data.bookmarkname];
+				bookmarkDict.bookmarks[newBookmarkName] ={};
+				bookmarkDict.bookmarks[newBookmarkName] = currentBookmark;
+				
+                //rename(id) bookmarks from nodes
+                //todo...
+                
+            }            
+        }
+    };
+    $("#"+bookmarkname+" .editbookmarkname").on("click",{bookmarkname:bookmarkname},EditCurrentBookmark);
+
+    
+    //cancel edit current bookmark
+    var CancelEditCurrentbookmark = function(event){
+        var $buttonObj = $("#"+event.data.bookmarkname+" .editbookmarkname");
+        $buttonObj.text("edit");
+        $("#"+event.data.bookmarkname+" .editbookmarkshow").css("display","none");
+        $("#"+event.data.bookmarkname+" .workbookmarkshow").css("display","");
+    };
+    $("#"+bookmarkname+" .cancelbookmarkname").on("click",{bookmarkname:bookmarkname},CancelEditCurrentbookmark);
+    
+    
+        
+    //current bookmark selected
+    var CurrentBookmarkSelected = function(event){
+        currentSelectedBookmark = event.data.bookmarkname;
+        //console.log(currentSelectedBookmark);
+        $(".bookmark").css("background","");
+        $("#"+event.data.bookmarkname + " .bookmark").css("background","yellow");
+        
+        $("#message").text(event.data.bookmarkname+ " selected");
+        
+    };
+    $("#"+bookmarkname).on("click",{bookmarkname:bookmarkname},CurrentBookmarkSelected);
+    
+    
+    $("#newbookmarkname").val("");
+    $("#message").text("success");
+    
+	bookmarkDict.bookmarks[bookmarkname] ={};
+	
+});
+
+
+
+
+//delete content from the message text
+
+$("#newbookmarkname,#newcolor,#message")
+    .on("focus click",function(){
+    $("#message").text("");
+});
---
+++ visualizations/GraphVisualization/Libs/ForceGraph.js
@@ -0,0 +1,188 @@
+
+var FGraph = function(){
+	var core = null;
+	
+	var nodeWorks = null;
+	var linkWorks = null;
+	var markerWorks = null;
+	var clusterWorks = null;
+	var workFunc = null;		
+		
+	var graphData = null;
+	
+	var currentObjectContent = null;
+	
+	
+	
+	var ToObject = function(){currentObjectContent = oC;return currentObjectContent;};	
+	
+	var ToNode = function(){currentObjectContent = oC.Node;return currentObjectContent;};
+	var ToLink = function(){currentObjectContent = oC.Link;return currentObjectContent;};
+	var ToMarker = function(){currentObjectContent = oC.Marker;return currentObjectContent;};
+	var ToGraph = function(){currentObjectContent = oC.Graph;return currentObjectContent;};
+
+	var ToNodeSubElement = function(){currentObjectContent = oC.Node.ChangeSubElement;return currentObjectContent;};
+	var ToNodeCluster = function(){currentObjectContent = oC.Node.Cluster;return currentObjectContent;};
+	var ToLinkSubElement = function(){currentObjectContent = oC.Link.ChangeSubElement;return currentObjectContent;};
+	var ToMarkerSubElement = function(){currentObjectContent = oC.Marker.ChangeSubElement;return currentObjectContent;};	
+		
+	var oC = {
+		//Init graph
+		InitGraph:function(chartIdParam){
+			core =new Core();
+			core.InitCore(chartIdParam);
+			//graphData = core.graphData;
+			
+			nodeWorks = new NodeClass(core);
+			linkWorks = new LinkClass(core);
+			markerWorks = new MarkerClass(core);
+			clusterWorks = new ClusterClass(core);
+			workFunc = new WorkFunctions(core);
+
+			currentObjectContent = oC;
+		},
+
+		Test1:function(){
+			return currentObjectContent;
+		},
+		
+		
+		To:{
+			Object:ToObject,
+			Node:ToNode,Link:ToLink,
+			Graph:ToGraph,Marker:ToMarker
+		},
+		
+		Node:{
+			To:{
+				SubElement:ToNodeSubElement,Object:ToObject,Cluster:ToNodeCluster
+			},
+			
+			Add:function(nodeId,nodeObj){
+				nodeWorks.Add(nodeId,nodeObj);return currentObjectContent;
+			},	
+			Change:function(nodeId,param){
+				nodeWorks.Change(nodeId,param);return currentObjectContent;
+			},
+			Delete:function(nodeId){
+				nodeWorks.Delete(nodeId);return currentObjectContent;
+			},
+			ChangeSubElement:{
+				To:{
+					Object:ToObject,Node:ToNode
+				},
+				Delete:function(nodeId,deleteSubNodeId){
+					nodeWorks.ChangeSubElement.Delete(nodeId,deleteSubNodeId);return currentObjectContent;
+				},
+				Add:function(nodeId,newSubNodeId,svgSubElement){
+					nodeWorks.ChangeSubElement.Add(nodeId,newSubNodeId,svgSubElement);return currentObjectContent;
+				},
+				Change:function(nodeId,subNodeId,param){
+					nodeWorks.ChangeSubElement.Change(nodeId,subNodeId,param);return currentObjectContent;
+				}				
+			},
+			Cluster:{
+				To:{
+					Object:ToObject,Node:ToNode
+				},
+				Add:function(clusterName,nodeName){
+					clusterWorks.ToCluster(clusterName,nodeName);return currentObjectContent;
+				},
+				Delete:function(clusterName){
+					clusterWorks.AwayCluster(clusterName);return currentObjectContent;
+				},
+			}
+		},
+		Link:{
+			To:{
+				SubElement:ToLinkSubElement,Object:ToObject
+			},
+			Add:function(sourceNodeId,targetNodeId,linkId,linkObj){
+				linkWorks.Add(sourceNodeId,targetNodeId,linkId,linkObj);return currentObjectContent;
+			},	
+			Change:function(linkId,param){
+				linkWorks.Change(linkId,param);return currentObjectContent;
+			},
+			Delete:function(linkId){
+				linkWorks.Delete(linkId);return currentObjectContent;
+			},
+			ChangeSubElement:{
+				To:{
+					Object:ToObject,Link:ToLink//,Node:ToNode
+				},
+				Delete:function(linkId,deleteSubLinkId){
+					linkWorks.ChangeSubElement.Delete(linkId,deleteSubLinkId);return currentObjectContent;
+				},
+				Add:function(linkId,newSubLinkId,svgSubElement){
+					linkWorks.ChangeSubElement.Add(linkId,newSubLinkId,svgSubElement);return currentObjectContent;
+				},
+				Change:function(linkId,subLinkId,param){
+					linkWorks.ChangeSubElement.Change(linkId,subLinkId,param);return currentObjectContent;
+				}				
+			}
+		},
+		Marker:{
+			To:{
+				SubElement:ToMarkerSubElement,Object:ToObject
+			},
+			Add:function(markerId,markerObj){
+				markerWorks.Add(markerId,markerObj);return currentObjectContent;
+			},	
+			Change:function(markerId,param){
+				markerWorks.Change(markerId,param);return currentObjectContent;
+			},
+			Delete:function(markerId){
+				markerWorks.Delete(markerId);return currentObjectContent;
+			},
+			ChangeSubElement:{
+				To:{
+					Object:ToObject,Marker:ToMarker
+				},
+				Delete:function(markerId,newSubMarkerId){
+					markerWorks.ChangeSubElement.Delete(markerId,newSubMarkerId);return currentObjectContent;
+				},
+				Add:function(markerId,newSubMarkerId,svgSubElement){
+					markerWorks.ChangeSubElement.Add(markerId,newSubMarkerId,svgSubElement);return currentObjectContent;
+				},
+				Change:function(markerId,newSubMarkerId,param){
+					markerWorks.ChangeSubElement.Change(markerId,newSubMarkerId,param);return currentObjectContent;
+				}				
+			}
+		},
+		
+		Graph:{
+			To:{
+				Object:ToObject
+			},
+			Delete:function(){
+				workFunc.DeleteGraph();return currentObjectContent;
+			},
+			Import:function(data){
+				workFunc.ImportData(data);return currentObjectContent;
+			},
+			SetOptions:function(options){
+				workFunc.SetOptions(options);return currentObjectContent;
+			},
+			SetScaleTrans:function(scaleTrans){
+				workFunc.SetScaleTrans(scaleTrans);return currentObjectContent;
+			},
+			ReDraw:function(){
+				workFunc.Redraw();return currentObjectContent;
+			},
+			ReOption:function(){
+				workFunc.Reoption();return currentObjectContent;
+			},	
+			ReScale:function(){
+				workFunc.Rescale();return currentObjectContent;
+			},
+			GetGraphData:function(){
+				return core.graphData;
+			},
+			GetForceObj:function(){
+				return core.force;
+			}
+		}
+		
+		
+	}
+return oC;};
