6c58cd5b825cc051901550948ce74e642f9fbc24
schloett <joerg.schloetterer@uni-passau.de>
2014-06-12 09:37:54 +0200

    switched to stable server

--- background/config.js
+++ background/config.js
@@ -1,7 +1,7 @@
 var EEXCESS = EEXCESS || {};
 
 (function() {
-    localStorage['PP_BASE_URI'] = 'http://eexcess-dev.joanneum.at/eexcess-privacy-proxy/';
+    localStorage['PP_BASE_URI'] = 'http://eexcess.joanneum.at/eexcess-privacy-proxy/';
 
     // init profile
     if (!localStorage['profile.uuid']) {
