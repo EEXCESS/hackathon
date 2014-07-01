c2830eadeb2a916a109abbda84cfc94cf3cce8f3
Jörg Schlötterer <joerg.schloetterer@uni-passau.de>
2014-07-01 14:57:46 +0200

    reset pp-url

--- background/init.js
+++ background/init.js
@@ -1,7 +1,7 @@
 var EEXCESS = EEXCESS || {};
 
 (function() {
-    localStorage['PP_BASE_URI'] = 'http://localhost:8080/eexcess-privacy-proxy/';
+    localStorage['PP_BASE_URI'] = 'http://eexcess-dev.joanneum.at/eexcess-privacy-proxy/';
 
     // init profile
     if (!localStorage['profile.uuid']) {
