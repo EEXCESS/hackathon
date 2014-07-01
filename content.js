b4c7206d635e1c3776dbac6fd012c64dba1840a4
Jörg Schlötterer <joerg.schloetterer@uni-passau.de>
2014-07-01 14:54:36 +0200

    removed unused code

--- content.js
+++ content.js
@@ -1,20 +1,3 @@
-/**
- * Extend the String object with a 'startsWith' method
- */
-if (typeof String.prototype.startsWith !== 'function') {
-    /**
-     * Checks, if a string-object starts with the provided term
-     * @global
-     * @param {String} str the term to check
-     * @returns {Boolean} true, if the string starts with the provided term, otherwise false
-     */
-    String.prototype.startsWith = function(str) {
-        return this.slice(0, str.length) === str;
-    };
-}
-
-
-
 var EEXCESS = EEXCESS || {};
 /**
  * Identifier of the chrome extension
@@ -27,7 +10,6 @@ EEXCESS.extID = chrome.i18n.getMessage('@@extension_id');
  * @type Boolean
  */
 EEXCESS.widgetVisible = false;
-
 
 /**
  * Changes the widget's visibility to the provided value.
@@ -85,22 +67,11 @@ chrome.runtime.onMessage.addListener(
                         afterShow: function() {
                             // log opening the page's preview in the background script
                             chrome.runtime.sendMessage(EEXCESS.extID, {method: {parent: 'logging', func: 'openedRecommendation'}, data: request.data});
-							console.log("test go");
-							console.log(request.data);
-						},
-						beforeClose:function(){
-							// log closing the page's preview in the background script
-							console.log("test");
-							console.log(request.data);
-							console.log(EEXCESS.extID);
-					
-                            //chrome.runtime.sendMessage(EEXCESS.extID, {method: {parent: 'logging', func: 'closedRecommendation'}, data: request.data});
-
-						},
+                        },
                         afterClose: function(evt) {
                             // log closing the page's preview in the background script
                             chrome.runtime.sendMessage(EEXCESS.extID, {method: {parent: 'logging', func: 'closedRecommendation'}, data: request.data});
-						}
+                        }
                     }).trigger('click');
                     break;
                 case 'useResource':
@@ -115,107 +86,11 @@ chrome.runtime.onMessage.addListener(
                     }
                     break;
                 case 'getTextualContext':
-//                    /*
-//                     * Retrieves the text of dom-nodes, currently visible in the
-//                     * viewport and sends it to the background script.
-//                     * If a selection is present, this selection is sent as well.
-//                     */
-//                    // viewport
-//                    var docViewTop = $(window).scrollTop();
-//                    var docViewBottom = docViewTop + $(window).height();
-//                    /*
-//                     * Generic filtering, using only nodes inside the current
-//                     * viewport, excluding 'style', 'script', 'noscript' and 
-//                     * hidden nodes
-//                     */
-//                    var root = document;
-//                    var filter = {acceptNode: function(node) {
-//                            var parent = $(node).parent();
-//                            var elemTop = parent.offset().top;
-//                            var elemBottom = elemTop + $(node).parent().height();
-//                            if ((elemBottom > docViewTop) && (elemTop < docViewBottom)) {
-//                                var nodeName = parent.prop('nodeName');
-//                                // exclude 'style', 'script' and 'noscript' nodes, as they 
-//                                var dataElement = (nodeName === 'STYLE' || nodeName === 'SCRIPT' || nodeName === 'NOSCRIPT');
-//                                if (!dataElement && !parent.is(':hidden')) {
-//                                    return NodeFilter.FILTER_ACCEPT;
-//                                }
-//                            }
-//                            return NodeFilter.FILTER_REJECT;
-//                        }};
-//
-//                    /*
-//                     * Specific filtering for wikipedia, considering only nodes
-//                     * within the content part of the page, excluding navigational
-//                     * menus.
-//                     */
-//                    var wikiContent = $('#mw-content-text').get()[0];
-//                    if (typeof wikiContent !== 'undefined') {
-//                        root = wikiContent;
-//                        filter = {acceptNode: function(node) {
-//                                if ($(node).parents('.dablink').length > 0) {
-//                                    return NodeFilter.FILTER_REJECT;
-//                                }
-//                                if ($(node).parents('#toc').length > 0) {
-//                                    return NodeFilter.FILTER_REJECT;
-//                                }
-//                                var parent = $(node).parent();
-//                                var elemTop = parent.offset().top;
-//                                var elemBottom = elemTop + parent.height();
-//                                if ((elemBottom > docViewTop) && (elemTop < docViewBottom)) {
-//                                    var nodeName = parent.prop('nodeName');
-//                                    // exclude 'style', 'script' and 'noscript' nodes, as they 
-//                                    var dataElement = (nodeName === 'STYLE' || nodeName === 'SCRIPT' || nodeName === 'NOSCRIPT');
-//                                    if (!dataElement && !parent.is(':hidden')) {
-//                                        return NodeFilter.FILTER_ACCEPT;
-//                                    }
-//                                } else {
-//                                    return NodeFilter.FILTER_REJECT;
-//                                }
-//                            }};
-//                    }
-//                    // tree walker with generic or wikipedia filter
-//                    var walker = document.createTreeWalker(
-//                            root,
-//                            NodeFilter.SHOW_TEXT,
-//                            filter
-//                            );
-//                    // concatenate text of respective nodes
-//                    var node;
-//                    var viewPortText = '';
-//                    while (node = walker.nextNode()) {
-//                        viewPortText += node.nodeValue;
-//                    }
-//                    /*
-//                     * The text inside the viewport is split into several 
-//                     * paragraphs, in order to extract only those parts,
-//                     * which contain relevant information (navigational items
-//                     * should be ignored for example). There seem to be several
-//                     * blank text nodes between dom elements (mostly linebreaks),
-//                     * thus, the text is splitted at at least two consecutive
-//                     * whitespace characters. Enforcing the extracted text parts
-//                     * to have a certain minimum length increases the probability
-//                     * to extract paragraphs, which consist of real sentences 
-//                     * and thus reveal contextual information
-//                     * TODO: do not apply filter here, but post filter the log?
-//                     */
-//                    var matches = viewPortText.match(/\s{2,}(\S*\s?\S)+/g);
-//                    var paragraphs = [];
-//                    for (var i = 0, len = matches.length; i < len; i++) {
-//                        var tmp = matches[i].trim();
-//                        if (tmp.length > 0) { // may be adjusted for filtering
-//                            if (tmp !== 'Annotate') { // filter out annotator
-//                                paragraphs.push(tmp);
-//                            }
-//                        }
-//                    }
                     sendResponse({selectedText: document.getSelection().toString(), url: document.URL});
                     break;
             }
         }
 );
-
-
 
 
 /**
@@ -231,9 +106,6 @@ EEXCESS.callBG = function(message, callback) {
         chrome.runtime.sendMessage(EEXCESS.extID, message);
     }
 };
-
-
-
 
 
 /*
