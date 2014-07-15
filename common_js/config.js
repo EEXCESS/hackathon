500ebe24b0e63fb1392d8ab498c7efa4b33f6b36
Jörg Schlötterer <joerg.schloetterer@uni-passau.de>
2014-07-15 18:10:20 +0200

    initial privacy policy settings

--- common_js/config.js
+++ common_js/config.js
@@ -1,6 +1,32 @@
 var EEXCESS = EEXCESS || {};
 
-EEXCESS.config = (function() {
+EEXCESS.config = (function() {    
+    // init privacy policy
+    if(typeof EEXCESS.storage.local('privacy.policy.address') === 'undefined') {
+        EEXCESS.storage.local('privacy.policy.address', 2);
+    }
+    if(typeof EEXCESS.storage.local('privacy.policy.birthdate') === 'undefined') {
+        EEXCESS.storage.local('privacy.policy.birthdate', 1);
+    }
+    if(typeof EEXCESS.storage.local('privacy.policy.firstname') === 'undefined') {
+        EEXCESS.storage.local('privacy.policy.firstname', 0);
+    }
+    if(typeof EEXCESS.storage.local('privacy.policy.gender') === 'undefined') {
+        EEXCESS.storage.local('privacy.policy.gender', 0);
+    }
+    if(typeof EEXCESS.storage.local('privacy.policy.history') === 'undefined') {
+        EEXCESS.storage.local('privacy.policy.history', 2);
+    }
+    if(typeof EEXCESS.storage.local('privacy.policy.lastname') === 'undefined') {
+        EEXCESS.storage.local('privacy.policy.lastname', 0);
+    }
+    if(typeof EEXCESS.storage.local('privacy.policy.topics') === 'undefined') {
+        EEXCESS.storage.local('privacy.policy.topics', 1);
+    }
+    if(typeof EEXCESS.storage.local('privacy.policy.uuid') === 'undefined') {
+        EEXCESS.storage.local('privacy.policy.uuid', 1);
+    }
+    
     var _PP_BASE_URI = 'http://eexcess-dev.joanneum.at/eexcess-privacy-proxy/api/v1/';
     var _LOG_RATING_URI = _PP_BASE_URI + 'log/rating';
     var _LOG_RVIEW_URI = _PP_BASE_URI + 'log/rview';
