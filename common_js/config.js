66d8866a319467440fe1e0cb955bd1d794a0cf23
Jörg Schlötterer <joerg.schloetterer@uni-passau.de>
2014-10-02 12:48:41 +0200

    merge cherry-picked source selection

--- common_js/config.js
+++ common_js/config.js
@@ -26,9 +26,8 @@ EEXCESS.config = (function() {
     if(typeof EEXCESS.storage.local('privacy.policy.uuid') === 'undefined') {
         EEXCESS.storage.local('privacy.policy.uuid', 1);
     }
-    
-    EEXCESS.storage.local('backend', 'fr-stable');
-    
+
+    var _FR_BASE_URI = 'http://eexcess.joanneum.at/eexcess-federated-recommender-web-service-1.0-SNAPSHOT/recommender/';
     var _PP_BASE_URI = 'http://eexcess.joanneum.at/eexcess-privacy-proxy/api/v1/';
     var _LOG_RATING_URI = _PP_BASE_URI + 'log/rating';
     var _LOG_RVIEW_URI = _PP_BASE_URI + 'log/rview';
@@ -46,6 +45,7 @@ EEXCESS.config = (function() {
         LOG_SHOW_HIDE_URI: _LOG_SHOW_HIDE_URI,
         DISAMBIGUATE_URI:_DISAMBIGUATE_URI,
         LOG_FACETSCAPE_URI:_LOG_FACETSCAPE_URI,
-        LOG_QUERY_ACTIVATED_URI: _LOG_QUERY_ACTIVATED_URI
+        LOG_QUERY_ACTIVATED_URI: _LOG_QUERY_ACTIVATED_URI,
+        FR_BASE_URI: _FR_BASE_URI
     };
 })();
