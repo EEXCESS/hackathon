5d5a3ec21fc4d456a448bc1d581fcadc98dfb89b
Jörg Schlötterer <joerg.schloetterer@uni-passau.de>
2014-10-16 16:18:29 +0200

    version + url change

--- common_js/config.js
+++ common_js/config.js
@@ -31,11 +31,11 @@ EEXCESS.config = (function() {
     }
     
     if(typeof EEXCESS.storage.local('backend') === 'undefined') {
-        EEXCESS.storage.local('backend','fr-devel');
+        EEXCESS.storage.local('backend','fr-stable');
     }
 
-    var _FR_BASE_URI = 'http://eexcess-dev.joanneum.at/eexcess-federated-recommender-web-service-1.0-SNAPSHOT/recommender/';
-    var _PP_BASE_URI = 'http://eexcess-dev.joanneum.at/eexcess-privacy-proxy/api/v1/';
+    var _FR_BASE_URI = 'http://eexcess.joanneum.at/eexcess-federated-recommender-web-service-1.0-SNAPSHOT/recommender/';
+    var _PP_BASE_URI = 'http://eexcess.joanneum.at/eexcess-privacy-proxy/api/v1/';
     var _LOG_RATING_URI = _PP_BASE_URI + 'log/rating';
     var _LOG_RVIEW_URI = _PP_BASE_URI + 'log/rview';
     var _LOG_RCLOSE_URI = _PP_BASE_URI + 'log/rclose';
