9941947cb07b07067c35d71e6d43ddce604320b7
gerwald <gerwald.tschinkel@interad.at>
2015-03-12 18:55:52 +0100

    Reset button temporarily implemented due to Problems with canceling the geovis Selection

--- visualizations/Vis-Template/Plugins/TestMiniViz.js
+++ visualizations/Vis-Template/Plugins/TestMiniViz.js
@@ -3,18 +3,17 @@
 	var TestMiniViz = {};
 	var $inner = null;
 
-	TestMiniViz.initialize = function(EEXCESSObj){
-		
+	TestMiniViz.initialize = function(EEXCESSObj){		
 		// load CSS
 		// load other needed scripts (require.js is available)
 	};
 
-	// filtes[].facet, from, to, type
 	TestMiniViz.draw = function(data, from, to, $container){
 		if (this.$inner == null){
 			this.$inner = $('<div>Hallo</div>').css('background-color', 'lightgrey').css('margin-top', '10px').css('padding-top', '10px').css('padding-bottom', '10px');		
 			$container.append(this.$inner);
 		}
+
 		this.$inner.html('Filter: ' + from + " - " + to);
 	};
 
